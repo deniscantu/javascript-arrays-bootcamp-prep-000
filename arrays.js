8f583fcf852df1ee8c871d9f3ebfb7ba79cb661e
Denis <denis@comebien.mx>
2018-01-05 04:34:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,7 @@ function addElementToBeginningOfArray (array,element) {
   array = [element,...array]
   return array
 }
+
+function destructivelyAddElementToBeginningOfArray (array,element) {
+  return = array.unshift (element)
+}
